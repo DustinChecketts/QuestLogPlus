d0f3202e19ef1eca8bb3f60351024de6fc17519e
Dustin Checketts <8584623+DustinChecketts@users.noreply.github.com>
2023-12-26 11:06:18 -0500

    Update QuestLogPlus.lua
    ChatGPT cleanup result

--- QuestLogPlus.lua
+++ QuestLogPlus.lua
@@ -3,10 +3,14 @@ local addOnName, ns = ...
 
 -- Create a cache for quest data
 local questCache = {}
+
 -- Define the level cap
 local LEVEL_CAP = 80
+
 -- Initialize addon data
 local addonData
+
+-- Default saved variables
 local defaultSavedVariables = {
     QXPdb = {},
 }
@@ -17,18 +21,15 @@ QuestLogPlus:RegisterEvent("ADDON_LOADED")
 
 -- Event handler for ADDON_LOADED event
 function QuestLogPlus:ADDON_LOADED(loadedAddOnName)
-    -- Check if the loaded addon is the intended one
     if loadedAddOnName == addOnName then
         -- Initialize SavedVariables
-        if not QXPdb then
-            QXPdb = {}
-        end
+        QXPdb = QXPdb or {}
         addonData = QXPdb
 
         -- Register events for quest-related actions
-        QuestLogPlus:RegisterEvent("QUEST_DETAIL")
-        QuestLogPlus:RegisterEvent("QUEST_ACCEPTED")
-        QuestLogPlus:RegisterEvent("QUEST_REMOVED")
+        self:RegisterEvent("QUEST_DETAIL")
+        self:RegisterEvent("QUEST_ACCEPTED")
+        self:RegisterEvent("QUEST_REMOVED")
 
         -- Hook into the QuestLog_Update function to customize quest display
         hooksecurefunc("QuestLog_Update", function()
@@ -61,9 +62,7 @@ function QuestLogPlus:QUEST_DETAIL()
 
     -- Cache quest XP if the ID and XP are valid
     if questID > 0 and questXP > 0 then
-        questCache[questID] = {
-            XP = questXP
-        }
+        questCache[questID] = {XP = questXP}
     end
 end
 
@@ -76,9 +75,7 @@ function QuestLogPlus:QUEST_ACCEPTED(questIndex, questID)
         -- If not in the cache, retrieve quest XP and add it to the addon data
         local questXP = GetRewardXP()
         if questXP > 0 then
-            addonData[questID] = {
-                XP = questXP
-            }
+            addonData[questID] = {XP = questXP}
         end
     end
 end
@@ -91,105 +88,64 @@ end
 
 -- Function to update the quest log display
 function QuestLogPlus:QuestLog_Update(addonName)
-    -- Variables for quest display customization
     local headerXP = {}
     local header
     local xpLevelTag = 'xp'
 
-    -- Check player's level for tag customization
     if UnitLevel("player") == LEVEL_CAP then
         xpLevelTag = '**'
     end
 
-    -- Get the number of quest log entries and quests
-    local numEntries, numQuests = GetNumQuestLogEntries();
-    local scrollOffset = HybridScrollFrame_GetOffset(QuestLogListScrollFrame);
-    local buttons = QuestLogListScrollFrame.buttons;
-    local buttonHeight = buttons[1]:GetHeight();
-    local displayedHeight = 0;
+    local numEntries, numQuests = GetNumQuestLogEntries()
+    local scrollOffset = HybridScrollFrame_GetOffset(QuestLogListScrollFrame)
+    local buttons = QuestLogListScrollFrame.buttons
+    local buttonHeight = buttons[1]:GetHeight()
+    local displayedHeight = 0
 
-    -- Variables for quest display details
-    local questIndex, questLogTitle, questTitleTag, questNumGroupMates, questNormalText, questHighlight, questCheck;
-    local questLogTitleText, level, questTag, isHeader, isCollapsed, isComplete, color;
-    local numPartyMembers, partyMembersOnQuest, tempWidth, textWidth;
+    local questIndex, questLogTitle, questTitleTag, questNumGroupMates, questNormalText, questHighlight, questCheck
+    local questLogTitleText, level, questTag, isHeader, isCollapsed, isComplete, color
+    local numPartyMembers, partyMembersOnQuest, tempWidth, textWidth
 
-    -- Determine the number of quests to be displayed based on the addon
     local numQuestsDisplayed
+
     if addonName == "QuestLogEx" then
         numQuestsDisplayed = QuestLogEx.db.global.maxQuestsDisplayed
     else
         numQuestsDisplayed = QUESTS_DISPLAYED
     end
 
-    -- Iterate over quests to customize their display
     for i=1, numQuestsDisplayed, 1 do
-        questLogTitle = buttons[i];
-        questIndex = i + scrollOffset;
-        questLogTitle:SetID(questIndex);
-        questTitleTag = questLogTitle.tag;
-        questNumGroupMates = questLogTitle.groupMates;
-        questCheck = questLogTitle.check;
+        questLogTitle = buttons[i]
+        questIndex = i + scrollOffset
+        questLogTitle:SetID(questIndex)
+        questTitleTag = questLogTitle.tag
+        questNumGroupMates = questLogTitle.groupMates
+        questCheck = questLogTitle.check
         questNormalText = questLogTitle.normalText
 
-        -- Get quest information for the current button
-        if ( questIndex <= numEntries ) then
-            questLogTitleText, level, questTag, isHeader, isCollapsed, isComplete, frequency, questID, startEvent, displayQuestID, isOnMap, hasLocalPOI, isTask, isBounty, isStory, isHidden, isScaling = GetQuestLogTitle(questIndex);
+        if questIndex <= numEntries then
+            questLogTitleText, level, questTag, isHeader, isCollapsed, isComplete, frequency, questID, startEvent, displayQuestID, isOnMap, hasLocalPOI, isTask, isBounty, isStory, isHidden, isScaling = GetQuestLogTitle(questIndex)
 
-            -- Set the quest tag
-            if ( isComplete and isComplete < 0 ) then
-                questTag = FAILED;
-            elseif ( isComplete and isComplete > 0 ) then
-                questTag = COMPLETE;
-            elseif ( frequency == LE_QUEST_FREQUENCY_DAILY ) then
-                if ( questTag ) then
-                    questTag = format(DAILY_QUEST_TAG_TEMPLATE, questTag);
-                else
-                    questTag = DAILY;
-                end
+            if isComplete and isComplete < 0 then
+                questTag = FAILED
+            elseif isComplete and isComplete > 0 then
+                questTag = COMPLETE
+            elseif frequency == LE_QUEST_FREQUENCY_DAILY then
+                questTag = questTag and format(DAILY_QUEST_TAG_TEMPLATE, questTag) or DAILY
             end
 
-            -- Customize quest title tag based on data availability
-            if ( questTag ) then
-                if QXPdb[questID] then
-                    questTitleTag:SetText(string.format("%d%s (%s)", QXPdb[questID].XP, xpLevelTag, questTag));
-                else
-                    questTitleTag:SetText("("..questTag..")");
-                end
-                -- Shrink text to accommodate quest tags without wrapping
-                tempWidth = 275 - 15 - questTitleTag:GetWidth();
+            if questTag then
+                local tagText = QXPdb[questID] and string.format("%d%s (%s)", QXPdb[questID].XP, xpLevelTag, questTag) or "("..questTag..")"
+                questTitleTag:SetText(tagText)
 
-                if ( QuestLogDummyText:GetWidth() > tempWidth ) then
-                    textWidth = tempWidth;
-                else
-                    textWidth = QuestLogDummyText:GetWidth();
-                end
-                questNormalText:SetWidth(tempWidth);
-                -- If there's a quest tag position check accordingly
-                questTitleTag:Show();
-                --questCheck:Hide();
+                tempWidth = 275 - 15 - questTitleTag:GetWidth()
+                textWidth = math.min(tempWidth, QuestLogDummyText:GetWidth())
+                questNormalText:SetWidth(tempWidth)
+                questTitleTag:Show()
             else
-                -- Customize quest title tag based on data availability
-                if QXPdb[questID] then
-                    questTitleTag:SetText(string.format("%d%s", QXPdb[questID].XP, xpLevelTag));
-
-                    -- Shrink text to accommodate quest tags without wrapping
-                    tempWidth = 275 - 15 - questTitleTag:GetWidth();
-
-                    if ( QuestLogDummyText:GetWidth() > tempWidth ) then
-                        textWidth = tempWidth;
-                    else
-                        textWidth = QuestLogDummyText:GetWidth();
-                    end
-                    questNormalText:SetWidth(tempWidth);
-                    -- If there's a quest tag position check accordingly
-                    questTitleTag:Show();
-                    --questCheck:Hide();
-                else
-                    questTitleTag:SetText("");
-                end
+                questTitleTag:SetText("")
             end
 
-            -- Resize the quest log title button
             QuestLogTitleButton_Resize(questLogTitle)
         end
     end
@@ -199,23 +155,21 @@ end
 function QuestLogPlus:UpdateQuestLevels()
     local numEntries, numQuests = GetNumQuestLogEntries()
 
-    -- Return if there are no quest entries
-    if (numEntries == 0) then return end
+    if numEntries == 0 then
+        return
+    end
 
-    -- Variables for quest level update
     local questIndex, questLogTitle, title, level, _, isHeader, questTextFormatted, questCheck
 
-    -- Iterate over displayed quests to update levels
     for i = 1, _G.QUESTS_DISPLAYED, 1 do
         questIndex = i + FauxScrollFrame_GetOffset(QuestLogListScrollFrame)
 
-        if (questIndex <= numEntries) then
+        if questIndex <= numEntries then
             questLogTitle = _G["QuestLogListScrollFrameButton"..i]
             questCheck = _G["QuestLogListScrollFrameButton"..i.."Check"]
             title, level, _, isHeader = GetQuestLogTitle(questIndex)
 
-            -- If it's not a header, format and display quest level
-            if (not isHeader) then
+            if not isHeader then
                 questTextFormatted = format("[%d] %s", level, title)
                 questLogTitle:SetText(questTextFormatted)
                 questLogTitle.normalText:SetWidth(265 - _G["QuestLogListScrollFrameButton"..i.."Tag"]:GetStringWidth())
@@ -223,7 +177,6 @@ function QuestLogPlus:UpdateQuestLevels()
                 questCheck:SetVertexColor(64/255, 224/255, 208/255)
                 questCheck:SetDrawLayer("ARTWORK")
             else
-                -- Hide check for header entries
                 questCheck:Hide()
             end
         end
@@ -231,12 +184,11 @@ function QuestLogPlus:UpdateQuestLevels()
 end
 
 -- Set the script for the main frame to handle events
-QuestLogPlus:SetScript("OnEvent", function (self, event, ...)
+QuestLogPlus:SetScript("OnEvent", function(self, event, ...)
     if self[event] then
         return self[event](self, ...)
     end
 
-    -- Save changes to SavedVariables when the player logs out
     if event == "PLAYER_LOGOUT" then
         QXPdb = addonData
     end
